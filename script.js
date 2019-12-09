b5f95780303b3b58d0c8a14a0b6797d43b29e84c
Wade Helquist <ZWhiteShadow@Yahoo.com>
2019-12-08 23:07:16 -0700

    Part Way into the game

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
-function changecolor(num, wait) {
+var challangeArray = [];
+var gamerTestArray = [];
+
+function start() {
+  challangeArray = [];
+  gamerTestArray = [];
+  nextRound();
+}
+
+function nextRound() {
+
+  // don't let user click on anything
+  document.getElementById('up').setAttribute( "onClick", "" );
+  document.getElementById('left').setAttribute( "onClick", "" );
+  document.getElementById('middle').setAttribute( "onClick", "" );
+  document.getElementById('right').setAttribute( "onClick", "" );
+  document.getElementById('down').setAttribute( "onClick", "" );
+  document.getElementById('message').innerHTML= "Pay Close Attention!";
+  var nextChallange = Math.floor(Math.random() * 5) + 1; // Randomly pick next challange up down left right or middle 5 options
+  challangeArray.push(nextChallange);
+  challangeTime(challangeArray);
+}
+
+function challangeTime(challangeArray) {
+  for (i = 0; i < challangeArray.length; i++) {
+    changeColor(challangeArray[i], 3000);
+  }
+
+  setTimeout(function(){
+  document.getElementById('message').innerHTML= "Click Center First";
+  document.getElementById('middle').setAttribute( "onClick", "answerTest()" );
+}, 6000);
+}
+
+function answerTest(answerTest){
+  setTimeout(function(){ document.getElementById('message').innerHTML= "Ok Now Copy What I Did"; }, 4000);
+
+  document.getElementById('up').setAttribute( "onClick", "answerTest(1)" );
+  document.getElementById('left').setAttribute( "onClick", "answerTest(2)" );
+  document.getElementById('middle').setAttribute( "onClick", "answerTest(3)" );
+  document.getElementById('right').setAttribute( "onClick", "answerTest(4)" );
+  document.getElementById('down').setAttribute( "onClick", "answerTest(5)" );
+
+changeColor(answerTest,1750);
+
+}
+
+function changeColor(num, wait) {
   var audio;
 
   if (num == 1) {
