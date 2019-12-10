608161ae1967a036a793fcd861efeef7c45ddafe
Wade Helquist <ZWhiteShadow@Yahoo.com>
2019-12-10 08:28:30 -0700

    Space between Loops
    Still a work in progress

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var challangeArray, gamerTestArray, testNum, score;
 
 function start() {
-  challangeArray = [];
+  challangeArray = [1,2,3,4,5,1,2,3,4,5];
   gamerTestArray = [];
   score = 0;
   nextRound();
@@ -9,7 +9,7 @@ function start() {
 
 function nextRound() {
 
-  testNum = 0;
+  gamerTestArray = [];
   // don't let user click on anything
   document.getElementById('up').removeAttribute("onclick");
   document.getElementById('left').removeAttribute("onclick");
@@ -20,13 +20,15 @@ function nextRound() {
   document.getElementById('message').innerHTML = "Pay Close Attention!";
   var nextChallange = Math.floor(Math.random() * 5) + 1; // Randomly pick next challange up down left right or middle 5 options
   challangeArray.push(nextChallange);
-  challangeTime(challangeArray);
-}
 
-function challangeTime(challangeArray) {
-  for (i = 0; i < challangeArray.length; i++) {
-    changeColor(challangeArray[i], 3500);
-  }
+//  https://borgs.cybrilla.com/tils/javascript-for-loop-with-delay-in-each-iteration-using-iife/
+for (var i = 0; i < challangeArray.length; i++) {
+  (function (i) {
+    setTimeout(function () {
+      changeColor(challangeArray[i],1000);
+     }, 1250*i);
+    })(i);
+   };
 
   setTimeout(function() {
     document.getElementById('message').innerHTML = "Click Center First";
@@ -50,11 +52,17 @@ function answerTest(answerTest) {
   console.log(gamerTestArray[testNum] + " - " + challangeArray[testNum]);
 
   if (gamerTestArray[testNum] == challangeArray[testNum]) {
-    changeColor(answerTest);
+    changeColor(answerTest,1000);
     testNum += 1;
-    score += 1; // Change so score only increaes if past previous round;
     document.getElementById('message2').innerHTML = "Your Score: " + score;
-    nextRound(); // Change so that it only switches rounds until after all  have been selected
+
+    if(gamerTestArray.length == challangeArray.length){
+    score += 1; 
+    setTimeout(function() {
+    nextRound(); 
+    }, 6000);
+    }
+
   } else {
     var audio;
     audio = new Audio('wrong.wav');
@@ -66,14 +74,16 @@ function answerTest(answerTest) {
 function changeColor(num, wait) {
 
   if (num == 1) {
+ 
     audio = new Audio('a.wav');
     audio.play();
     document.getElementById("up").style.borderBottom = "9.375em solid darkred";
-    setTimeout(function() {
+      setTimeout(function() {
       document.getElementById("up").style.borderBottom = "9.375em solid #FF2400";
     }, wait);
   }
   if (num == 2) {
+  
     audio = new Audio('b.wav');
     audio.play();
     document.getElementById("left").style.borderRight = "9.375em solid chocolate";
@@ -82,6 +92,7 @@ function changeColor(num, wait) {
     }, wait);
   }
   if (num == 3) {
+
     audio = new Audio('c.wav');
     audio.play();
     document.getElementById("middle").style.backgroundColor = "#999900";
@@ -90,6 +101,7 @@ function changeColor(num, wait) {
     }, wait);
   }
   if (num == 4) {
+
     audio = new Audio('d.wav');
     audio.play();
     document.getElementById("right").style.borderLeft = "9.375em solid darkgreen";
@@ -98,6 +110,7 @@ function changeColor(num, wait) {
     }, wait);
   }
   if (num == 5) {
+
     audio = new Audio('e.wav');
     audio.play();
     document.getElementById("down").style.borderTop = "9.375em solid darkblue";
