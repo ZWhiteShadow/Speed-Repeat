53daf6cd2274e707de0b676d8427f20050167aff
Wade Helquist <ZWhiteShadow@Yahoo.com>
2019-12-10 22:58:19 -0700

    Timing and speeding up!

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 var challangeArray, gamerTestArray, testNum, score;
 
 function start() {
-  document.getElementById("title").style.fontSize = "3em";
-  document.getElementById("message").style.fontSize = "2em";
-  document.getElementById("message2").style.fontSize = "2em";
   document.getElementById('title').innerHTML = "Speed Repeat";
   challangeArray = [];
   gamerTestArray = [];
@@ -21,56 +18,58 @@ function clearClick() {
   document.getElementById('middle').removeAttribute("onclick"); 
 }
 
-function nextRound() {
+function displayChallange(){
+  //  https://borgs.cybrilla.com/tils/javascript-for-loop-with-delay-in-each-iteration-using-iife/
+  for (var i = 0; i < challangeArray.length; i++) {
+    (function (i) {
+      setTimeout(function () {
+        changeColor(challangeArray[i],750);
+       }, (250 + (1000 - i * 50))* i );
+      })(i);
+      console.log((250 + (1000 - i * 50)));
+     };
+ 
+}
 
+function nextRound() {
+  clearClick();
   gamerTestArray = [];
   testNum = 0;
-  clearClick();
   document.getElementById('message').innerHTML = "Pay Close Attention!";
   var nextChallange = Math.floor(Math.random() * 5) + 1; // Randomly pick next challange up down left right or middle 5 options
   challangeArray.push(nextChallange);
 
-//  https://borgs.cybrilla.com/tils/javascript-for-loop-with-delay-in-each-iteration-using-iife/
-for (var i = 0; i < challangeArray.length; i++) {
-  (function (i) {
-    setTimeout(function () {
-      changeColor(challangeArray[i],1000);
-     }, 1250*i);
-    })(i);
-   };
+  displayChallange();
 
   setTimeout(function() {
   document.getElementById('message').innerHTML = "Ok Now Copy What I Did";
-  }, (challangeArray.length)*1250);
   document.getElementById('up').setAttribute("onClick", "answerTest(1)");
   document.getElementById('left').setAttribute("onClick", "answerTest(2)");
   document.getElementById('middle').setAttribute("onClick", "answerTest(3)");
   document.getElementById('right').setAttribute("onClick", "answerTest(4)");
   document.getElementById('down').setAttribute("onClick", "answerTest(5)");
+}, (challangeArray.length)*(250 + (1000 - score * 50)));
+
 }
 
 function answerTest(answerTest) {
   gamerTestArray.push(answerTest);
 
-  console.log(gamerTestArray[testNum] + " - " + challangeArray[testNum]);
-
   if (gamerTestArray[testNum] == challangeArray[testNum]) {
-    changeColor(answerTest,1000);
+    changeColor(answerTest,750);
     testNum += 1;
 
     if(gamerTestArray.length == challangeArray.length){
     score += 1; 
     document.getElementById('message2').innerHTML = "Your Score: " + score;
+    clearClick();
     setTimeout(function() {
     nextRound(); 
-    }, 4000);
+    }, 3000);
     }
 
   } else {
     clearClick();
-    document.getElementById("title").style.fontSize = "4em";
-    document.getElementById("message").style.fontSize = "3em";
-    document.getElementById("message2").style.fontSize = "4em";
     var audio;
     audio = new Audio('wrong.wav');
     audio.play();
