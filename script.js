98be072ff5d4848dabc615758d3a51172107261d
Wade Helquist <ZWhiteShadow@Yahoo.com>
2019-12-10 20:58:11 -0700

    Finishing Touches!

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 var challangeArray, gamerTestArray, testNum, score;
 
 function start() {
+  document.getElementById("title").style.fontSize = "3em";
+  document.getElementById("message").style.fontSize = "2em";
+  document.getElementById("message2").style.fontSize = "2em";
+  document.getElementById('title').innerHTML = "Speed Repeat";
   challangeArray = [];
   gamerTestArray = [];
   score = 0;
+  document.getElementById('message2').innerHTML = "Your Score: " + score;
   nextRound();
+}
+
+function clearClick() {
+   // don't let user click on anything
+  document.getElementById('up').removeAttribute("onclick");
+  document.getElementById('left').removeAttribute("onclick");
+  document.getElementById('right').removeAttribute("onclick");
+  document.getElementById('down').removeAttribute("onclick");
+  document.getElementById('middle').removeAttribute("onclick"); 
 }
 
 function nextRound() {
 
   gamerTestArray = [];
-  // don't let user click on anything
-  document.getElementById('up').removeAttribute("onclick");
-  document.getElementById('left').removeAttribute("onclick");
-  document.getElementById('right').removeAttribute("onclick");
-  document.getElementById('down').removeAttribute("onclick");
-  document.getElementById('middle').removeAttribute("onclick");
-
+  testNum = 0;
+  clearClick();
   document.getElementById('message').innerHTML = "Pay Close Attention!";
   var nextChallange = Math.floor(Math.random() * 5) + 1; // Randomly pick next challange up down left right or middle 5 options
   challangeArray.push(nextChallange);
@@ -31,14 +40,8 @@ for (var i = 0; i < challangeArray.length; i++) {
    };
 
   setTimeout(function() {
-    document.getElementById('message').innerHTML = "Click Center First";
-    document.getElementById('middle').setAttribute("onClick", "copyMe()");
-  }, 5000);
-}
-
-function copyMe() {
   document.getElementById('message').innerHTML = "Ok Now Copy What I Did";
-
+  }, (challangeArray.length)*1250);
   document.getElementById('up').setAttribute("onClick", "answerTest(1)");
   document.getElementById('left').setAttribute("onClick", "answerTest(2)");
   document.getElementById('middle').setAttribute("onClick", "answerTest(3)");
@@ -54,19 +57,27 @@ function answerTest(answerTest) {
   if (gamerTestArray[testNum] == challangeArray[testNum]) {
     changeColor(answerTest,1000);
     testNum += 1;
-    document.getElementById('message2').innerHTML = "Your Score: " + score;
 
     if(gamerTestArray.length == challangeArray.length){
     score += 1; 
+    document.getElementById('message2').innerHTML = "Your Score: " + score;
     setTimeout(function() {
     nextRound(); 
-    }, 6000);
+    }, 4000);
     }
 
   } else {
+    clearClick();
+    document.getElementById("title").style.fontSize = "4em";
+    document.getElementById("message").style.fontSize = "3em";
+    document.getElementById("message2").style.fontSize = "4em";
     var audio;
     audio = new Audio('wrong.wav');
     audio.play();
+    document.getElementById('title').innerHTML = "GAME OVER!";
+    document.getElementById('message').innerHTML = "Click Center to Play Again";
+    document.getElementById('middle').setAttribute("onClick", "start()");
+    return;
   }
 
 }
